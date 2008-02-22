56569ce440d6613caf81189e1fd4516ec2c4c213
aaronchi <aaronchi@1bc9508b-f83e-0410-b78e-05f979d4f8d9>
2008-02-22 20:10:14 +0000

    fix blind
    git-svn-id: https://ennerchi.googlecode.com/svn/trunk/plugins/jrails@25 1bc9508b-f83e-0410-b78e-05f979d4f8d9

--- javascripts/sources/jrails.js
+++ javascripts/sources/jrails.js
@@ -86,7 +86,7 @@
       return this;
     },
     BlindRight : function(speed, callback) {
-      this.show({'blind', { direction: 'horizontal' }, speed, callback); 
+      this.show('blind', { direction: 'horizontal' }, speed, callback); 
       return this;
     },
     BlindLeft : function(speed, callback) {
