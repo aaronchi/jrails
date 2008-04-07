dc12d14d80e46e8a4216e229312edf2c03598054
aaronchi <aaronchi@1bc9508b-f83e-0410-b78e-05f979d4f8d9>
2008-04-07 21:53:36 +0000

    quote datatype
    git-svn-id: https://ennerchi.googlecode.com/svn/trunk/plugins/jrails@40 1bc9508b-f83e-0410-b78e-05f979d4f8d9

--- lib/jrails.rb
+++ lib/jrails.rb
@@ -84,7 +84,7 @@ module ActionView
         js_options['url'] = "'#{url_for(url_options)}'"
         js_options['async'] = options[:type] != :synchronous        
         js_options['type'] = options[:method] ? method_option_to_s(options[:method]) : ( options[:form] ? "'post'" : nil )
-        js_options['dataType'] = options[:datatype] ? options[:datatype] : (options[:update] ? nil : "'script'")
+        js_options['dataType'] = options[:datatype] ? "'#{options[:datatype]}'" : (options[:update] ? nil : "'script'")
         
         if options[:form]
           js_options['data'] = "$.param($(this).serializeArray())"
