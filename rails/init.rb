fd98ab64470dc8785ab7f7658b7276abe1246154
unknown <Aaron@.(none)>
2009-08-05 09:13:21 -0700

    documentation cleanup

--- rails/init.rb
+++ rails/init.rb
@@ -1,14 +1,14 @@
 # ====
-# The following variables can be changed by creating an initializer in config/initializers/jrails.rb
+# The following options can be changed by creating an initializer in config/initializers/jrails.rb
 # ====
-# JQUERY_VAR
+# ActionView::Helpers::PrototypeHelper::JQUERY_VAR
 # jRails uses jQuery.noConflict() by default
-# to use the regualr jQuery syntax, use:
+# to use the regular jQuery syntax, use:
 # ActionView::Helpers::PrototypeHelper::JQUERY_VAR = '$'
 ActionView::Helpers::PrototypeHelper::JQUERY_VAR = 'jQuery'
 
-# DISABLE_JQUERY_FORGERY_PROTECTION
-# Set this to uncomment to disable forgery protection in ajax calls
+# ActionView::Helpers::PrototypeHelper:: DISABLE_JQUERY_FORGERY_PROTECTION
+# Set this to disable forgery protection in ajax calls
 # This is handy if you want to using caching with ajax by injecting the forgery token via another means
 # for an example, see http://henrik.nyh.se/2008/05/rails-authenticity-token-with-jquery
 # ActionView::Helpers::PrototypeHelper::DISABLE_JQUERY_FORGERY_PROTECTION = true
